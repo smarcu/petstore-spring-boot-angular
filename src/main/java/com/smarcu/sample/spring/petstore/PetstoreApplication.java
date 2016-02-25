37b1ed06b021db22fe59c8b7b55e76ed41cb2dbc
Silviu Marcu <Silviu.Marcu@intelliware.com>
2016-02-25 11:59:08 -0500

    Initial project

---
+++ src/main/java/com/smarcu/sample/spring/petstore/PetstoreApplication.java
@@ -0,0 +1,12 @@
+package com.smarcu.sample.spring.petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstoreApplication.class, args);
+	}
+}
