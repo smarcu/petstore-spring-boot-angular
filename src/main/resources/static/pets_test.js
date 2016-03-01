073cc767cf9ba03d4be16d75f46fc16f50009396
smarcu <smarcu@gmail.com>
2016-03-01 16:34:26 -0500

    update angular test

--- src/main/resources/static/pets_test.js
+++ src/main/resources/static/pets_test.js
@@ -3,14 +3,33 @@
 describe('myApp.pets module', function() {
 
   beforeEach(module('myApp.pets'));
+  
+  var $controller;
+  
+  beforeEach(inject(function(_$controller_){
+	  $controller = _$controller_;
+  }));
 
   describe('pets controller', function(){
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var petsCtrl = $controller('PetsCtrl');
-      expect(petsCtrl).toBeDefined();
-    }));
+    it('should ....', function() {
+	  var $scope = {};
+	  var $log = {};
+	  var Pets = {
+		getPets() {
+			return [{id:1, name:'cat'}];
+		},
+		getCategories() {
+			return [{id:1, name:'cat1'}, {id:2, name:'cat2'}]
+		}
+	  };
+	  var petsCtrl = $controller('PetsCtrl', {$scope:$scope, $log:$log, Pets:Pets});
+	  expect(petsCtrl).toBeDefined();
+	  
+	  expect($scope.petTags).toBe("tag1,tag2");
+
+      
+    });
 
   });
 });
