23c246b8e7364f1214a5d826bf11f7e92e307df0
Silviu Marcu <Silviu.Marcu@intelliware.com>
2016-03-08 15:39:12 -0500

    add more e2e tests

--- src/test/javascript/e2e/pets_spec.js
+++ src/test/javascript/e2e/pets_spec.js
@@ -20,5 +20,37 @@ describe('Protractor Demo App', function() {
 		    expect(element(by.id('addForm')).isDisplayed()).toBeTruthy();
 		  });
 	  
-	  
+
+	  it('should add pet', function() {
+		    browser.get('');
+
+		    element(by.id('addPetButton')).click();
+
+		    expect(element(by.id('addForm')).isDisplayed()).toBeTruthy();
+		    
+		    var generatedName = 'test'+Date.now();
+		    var imgUrl = 'testUrl';
+		    var inputTags = 'tag1,tag2';
+		    
+		    element(by.model('pet.name')).clear().sendKeys(generatedName);
+		    element(by.model('petImgUrl')).clear().sendKeys(imgUrl);
+		    element(by.model('petTags')).clear().sendKeys(inputTags);
+		    element(by.cssContainingText('option', 'pending')).click();
+		    
+		    element(by.buttonText('Add Pet')).click();
+		    
+		    browser.waitForAngular();
+		    
+		    // search for the new pet
+		    element.all(by.repeater('pet in pets')).then(function(pets) {
+			    var titleElement = pets[pets.length-1].element(by.className('petName'));
+			    expect(titleElement.getText()).toEqual(generatedName);
+			    var deleteButton = pets[pets.length-1].element(by.className('deleteButton'));
+			    
+			    deleteButton.click();
+
+		    });
+		    
+		  });
+
 });
